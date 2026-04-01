55e88a8ec8624eb44a0b08e625a2e1f97efa72e3
Theo William De Freitas Justino - FATEC - Cuiaba/MT <theo.justino@alunosenai.mt>
2026-03-31 21:27:32 -0400

    Exercício 4

--- Lista/src/App.java
+++ Lista/src/App.java
@@ -11,7 +11,7 @@ public class App {
         listaCarros.add(new Carro("Marca2", "Modelo2", 2016));
         listaCarros.add(new Carro("Marca3", "Modelo3", 2017));
         listaCarros.add(new Carro("Marca4", "Modelo4", 2018));
-        listaCarros.add(new Carro("Marca5", "Modelo5", 2019));
+        listaCarros.add(new Carro("Marca5", "Modelo5", 2027));
 
         for (Carro car : listaCarros){
             App.exibirCarro(car);
@@ -21,13 +21,13 @@ public class App {
     public static void exibirCarro(Carro car){
         // Esse método não retorna valor algum pois é um método void, apenas executa uma ação
         System.out.println("Carro:");
-        System.out.println(car.marca);
-        System.out.println(car.modelo);
-        System.out.println(car.ano);
+        System.out.println(car.getMarca());
+        System.out.println(car.getModelo());
+        System.out.println(car.getAno());
     }
 
     public static int idadeDoCarro(Carro car){
         // Esse método retorna o valor da idade do carro
-        return 2026 - car.ano;
+        return 2026 - car.getAno();
     }
 }
--- Lista/src/Carro.java
+++ Lista/src/Carro.java
@@ -1,11 +1,40 @@
 public class Carro {
-    String marca;
-    String modelo;
-    int ano;
+    private String marca;
+    private String modelo;
+    private int ano;
 
     Carro(String marca, String modelo, int ano){
         this.marca = marca;
         this.modelo = modelo;
+        setAno(ano);
+    }
+
+    public String getMarca(){
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo(){
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAno(){
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        if (ano < 1886 || ano > 2026) {
+            System.out.println("Erro: o ano deve estar entre 1886 e 2026");
+            return;
+        }
         this.ano = ano;
     }
+
 }
